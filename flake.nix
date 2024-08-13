3accee803a821360a58edb2cae125ac8653ada2d
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2024-08-13 14:35:00 +0200

    Remove unneeded input override

--- flake.nix
+++ flake.nix
@@ -14,7 +14,6 @@
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
 
     fenix = {
